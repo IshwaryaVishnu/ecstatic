0b0b7068f3ac734e150e9cf384441c4c001feb5e
IshwaryaVishnu <ishu27.v@gmail.com>
2020-10-16 16:49:20 +0530

    calculator

---
+++ calc.py
@@ -0,0 +1,35 @@
+#calculator
+
+
+#addition
+def add(x,y):
+    return x + y
+
+#subtraction
+def sub(x,y):
+    return x - y
+
+#multiplication
+def mul(x,y):
+    return x * y
+
+#division
+def div(x,y):
+    return x / y
+print ("This is calculator")
+print("1.addition")
+print("2.subtraction")
+print("3.multiplication")
+print("4.division")
+choice = input("choose from the options (1, 2, 3, 4): ")
+
+num1 = int(input("enter the first number"))
+num2 = int(input("enter the second number"))
+if choice =='1':
+    print(f'{num1} + {num2} = {add(num1, num2)}')
+if choice == '2':
+    print(f'{num1} - {num2} = {sub(num1, num2)}')
+if choice =='3':
+    print(f'{num1} * {num2} = {mul(num1, num2)}')
+if choice =='4':
+    print(f'{num1} / {num2} = {div(num1, num2)}')
